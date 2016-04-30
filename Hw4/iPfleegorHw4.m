d3e303b566357c9b81d3e1bcc069bef64266079f
Isaac Pfleegor <ipfleegor@eagles.ewu.edu>
2016-04-29 21:18:34 -0700

    comment code for grader

--- Hw4/iPfleegorHw4.m
+++ Hw4/iPfleegorHw4.m
@@ -102,10 +102,10 @@ x_t = sin(t).*r';
 
 M = [x_t;y_t];
 % Part A
+%create cloud of data
 r = 0.7 +(1.2 - 0.7).*rand(500,1);
 t = linspace(0,2*pi,500);
-
-
+%get x and y values using t
 x_t = cos(t).*r';
 y_t = sin(t).*r';
 figure
@@ -118,7 +118,7 @@ cov_M = cov(M');
 % get eigen values from cov matrix
 [V_e,S_e] = eig(cov_M);
 
-
+%get pricipcal component vectors
 a = V_e(1,:);
 a_plot = [0,0;a];
 b = V_e(2,:);
@@ -128,33 +128,25 @@ axis equal
 plot(a_plot, b_plot);
 
 title('problem 3, part a plot');
-%get pricipcal component vectors
-%  [U,S,V] = svd(M);
-%principal components in U
-% disp(U);
 
-
-
-
-%get x and y values from projected data
-% x_1 = M_1(1,:);
-% y_1 = M_1(2,:);
 
 %Part B
 
 %project values onto basic vectors
 
 %plot new data
-%  plot(x_1,y_1, 'o');
+
+%project data cloud onto principal components
 x_1= a'*a*M;
 x_2 = b'*b*M;
 figure
 hold on
+%plot each component on each vector
 plot(x_1(1,:), x_1(2,:),'o');
 plot(x_2(1,:), x_2(2,:),'o');
 title('problem 3, part b plot');
 %Part C
-
+%square x and y values
 x_t2 = x_t.^2;
 y_t2 = y_t.^2;
 figure
@@ -168,7 +160,8 @@ disp('================Problem 4 ==============');
 % Then approximate the unit magnitude
 % using the linear approximation with the following coefficients:
 % i) a = 0.94754, b = 0.39249: these values minimize the RMS error
-% ii) a = 0.96043, b = 0.39782: these values ensure that the max and min errors are balanced around 0.
+% ii) a = 0.96043, b = 0.39782: these values ensure that the max 
+%and min errors are balanced around 0.
 
 
 angles = 0:360/1200:360;
