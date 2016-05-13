e5be69f96b63f82d904885ae5c007a0e9ef90830
Isaac Pfleegor <ipfleegor@eagles.ewu.edu>
2016-05-12 17:34:42 -0700

    add graph titles

--- Hw6/ipfleegorHw6.m
+++ Hw6/ipfleegorHw6.m
@@ -17,7 +17,7 @@ file_id = fopen('molecule.txt', 'r');
 % 
 [Symbols, X, Y] = Ihp_parse_molecule(file_id);
 figure
-title('Atomic Symbol Plot');
+title('Problem 1: Atomic Symbol Plot');
 hold on
 for i = 1:length(Symbols)
     tmp_symbol = Symbols(:,i);
@@ -33,7 +33,7 @@ for i = 1:length(Symbols)
 end
 
  fclose(file_id);
-return
+
 disp('');
 disp('=============== Problem 2 ===============');
 
@@ -41,24 +41,24 @@ volume = 1:6; %in m^3 units
 pressure = [2494, 1247, 831, 623, 499, 416];%in kPa units
 
 figure
-val1 = interp1(volume, pressure, 3.8);
-plot(volume, pressure,'x', 3.8, val1, 'o');
-title('linear1');
+linear1 = interp1(volume, pressure, 3.8)
+plot(volume, pressure,'x', 3.8, linear1, 'o');
+title('Problem 2:linear1');
 
 figure
-val2 = interp1(volume, pressure, 3.8, 'spline');
-plot(volume, pressure,'x', 3.8, val2, 'o');
-title('spline1');
+spline1 = interp1(volume, pressure, 3.8, 'spline')
+plot(volume, pressure,'x', 3.8, spline1, 'o');
+title('Problem 2:spline1');
 
 figure
-val3 = interp1(pressure,volume, 1000);
-plot(volume,pressure,'x', val3, 1000, 'o');
-title('linear2');
+linear2 = interp1(pressure,volume, 1000)
+plot(volume,pressure,'x', linear2, 1000, 'o');
+title('Problem 2:linear2');
 
 figure
-val4 = interp1(pressure,volume, 1000, 'spline');
-plot(volume,pressure,'x', val4, 1000, 'o');
-title('spline2');
+spline2 = interp1(pressure,volume, 1000, 'spline')
+plot(volume,pressure,'x', spline2, 1000, 'o');
+title('Problem 2:spline2');
 
 disp('');
 disp('=============== Problem 3 ===============');
@@ -76,7 +76,7 @@ temp = mean(temp)
 
 figure
 plot(volume_inv, pressure);
-
+title('Problem 3 P vs 1/V');
 xlabel('1/V');
 ylabel('Pressure');
 
@@ -106,7 +106,7 @@ newy = A*(exp(-1*((newx-mu).^2)/(2*sigma.^2)));
 
 figure
 h = plot(x,y,'o', newx, newy, '-');
-
+title('Problem 4:1st data set');
 disp('Part B');
 
 %pre-condition the y2 data
@@ -131,7 +131,7 @@ A = exp(A0+mu.^2/(2*sigma.^2))
 %plot line of best fit
 figure
 plot(x2,y2_conditioned,'o',x2, exp(polyval(coef,x2)));
-
+title('Problem 4: 2nd data set');
 diary off
 
 
