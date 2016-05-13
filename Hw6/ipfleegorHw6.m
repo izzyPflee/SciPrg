642357b93c930400ae280604ac6912ec3c943f62
Isaac Pfleegor <ipfleegor@eagles.ewu.edu>
2016-05-12 17:10:24 -0700

    add molecule.txt parsing ability

--- Hw6/ipfleegorHw6.m
+++ Hw6/ipfleegorHw6.m
@@ -4,48 +4,60 @@
 %
 clc, clear, close all ;
 
+disp('Isaac Pfleegor - CSCD 409 Homework 6');
+disp('This assignment was tested in Matlab');
 
+diary ipfleegorHw6.txt
 disp('');
 disp('=============== Problem 1 ===============');
 % 
 % %read the file
 % 
-% file_id = fopen('molecule.txt', 'r');
+file_id = fopen('molecule.txt', 'r');
 % 
-% temp = fgetl(file_id);
-% 
-% while ischar(temp)
-%     disp(temp)
-%     temp = fgetl(file_id);
-% end
-% 
-% fclose(file_id);
+%call a function
+%TODO:create function
+temp = fgetl(file_id);
+begin_str = '@ATOM';
+while(strcmp(temp, begin_str) ~= 1)
+    temp = fgetl(file_id);
+end
+formatSpec = '%*d %2c %f %f %*f'; 
+sizeA = [2 inf];
+A = fscanf(file_id, formatSpec, sizeA);
+%return values
+Symbols = char(A(:,1:2:end));
+X = A(1,2:2:end);
+Y = A(2,2:2:end);
 
+ 
+ fclose(file_id);
+return
 disp('');
 disp('=============== Problem 2 ===============');
-% 
-% volume = 1:6; %in m^3 units
-% pressure = [2494, 1247, 831, 623, 499, 416];%in kPa units
-% 
-% figure
-% val1 = interp1(volume, pressure, 3.8);
-% plot(volume, pressure,'x', 3.8, val1, 'o');
-% title('linear1');
-% 
-% figure
-% val2 = interp1(volume, pressure, 3.8, 'spline');
-% plot(volume, pressure,'x', 3.8, val2, 'o');
-% title('spline1');
-% 
-% figure
-% val3 = interp1(pressure,volume, 1000);
-% plot(volume,pressure,'x', val3, 1000, 'o');
-% title('linear2');
-% 
-% figure
-% val4 = interp1(pressure,volume, 1000, 'spline');
-% plot(volume,pressure,'x', val4, 1000, 'o');
-% title('spline2');
+
+volume = 1:6; %in m^3 units
+pressure = [2494, 1247, 831, 623, 499, 416];%in kPa units
+
+figure
+val1 = interp1(volume, pressure, 3.8);
+plot(volume, pressure,'x', 3.8, val1, 'o');
+title('linear1');
+
+figure
+val2 = interp1(volume, pressure, 3.8, 'spline');
+plot(volume, pressure,'x', 3.8, val2, 'o');
+title('spline1');
+
+figure
+val3 = interp1(pressure,volume, 1000);
+plot(volume,pressure,'x', val3, 1000, 'o');
+title('linear2');
+
+figure
+val4 = interp1(pressure,volume, 1000, 'spline');
+plot(volume,pressure,'x', val4, 1000, 'o');
+title('spline2');
 
 disp('');
 disp('=============== Problem 3 ===============');
@@ -63,10 +75,10 @@ temp = mean(temp)
 
 figure
 plot(volume_inv, pressure);
+
 xlabel('1/V');
 ylabel('Pressure');
 
-return
 
 disp('');
 disp('=============== Problem 4 ===============');
@@ -119,6 +131,6 @@ A = exp(A0+mu.^2/(2*sigma.^2))
 figure
 plot(x2,y2_conditioned,'o',x2, exp(polyval(coef,x2)));
 
-
+diary off
 
 
