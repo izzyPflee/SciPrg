217a3e20511dd05379775e102400c4394cf96299
Isaac <ipfleegor@eagles.ewu.edu>
2016-05-18 12:56:50 -0700

    add problem 3 to hw7

--- Hw7/ipfleegorHw7.m
+++ Hw7/ipfleegorHw7.m
@@ -43,14 +43,54 @@ clc, clear, close all;
 % dfs(step_t,2,1,0);
 % title('1C: m=2,n=8, interpolated');
 
+% disp('');
+% disp('=========== Problem 2 ==========');
+% 
+% %Part A
+% pulse_wave = [zeros(1,5),ones(1,4),zeros(1,5)];
+% 
+% %plot in 1,1
+% % [a0,a,b] = dfs(pulse_wave,0,1,0);
+% 
+% X = fft(pulse_wave);
+% X_1 = fftshift(X);
+% 
+% 
+% fs = 1;
+% time_domain = -fs/2:1/length(X):fs/2-1/length(X);
+% 
+% plot(time_domain,abs(X_1),'-o');
+% 
+% % Part B
+% 
+% pulse_wave_2 = [zeros(1,10),ones(1,8),zeros(1,10)];
+% X = fft(pulse_wave_2);
+% X_2 = fftshift(X);
+% 
+% fs = 2;
+% time_domain = -fs/2:1/length(pulse_wave):fs/2-1/length(pulse_wave);
+% 
+% plot(time_domain, abs(X_2),'-o');
+% 
+% % [a0,a,b] = dfs(pulse_wave_2,0,1,0);
+% 
+% %Part C
+% 
+% pulse_wave = [zeros(1,5),ones(1,4),zeros(1,5)];
+% X = fft(pulse_wave, 256);
+% X_2 = fftshift(X);
+% fs = 1;
+% time_domain = -fs/2:1/length(X_2):fs/2-1/length(X_2);
+% 
+% plot(time_domain,abs(X_2),'-o');
+% return
+
 disp('');
-disp('=========== Problem 2 ==========');
+disp('=========== Problem 3 ==========');
 
-%Part A
-pulse_wave = [zeros(1,5),ones(1,4),zeros(1,5)];
-
-%plot in 1,1
-% [a0,a,b] = dfs(pulse_wave,0,1,0);
+pulse_wave = [zeros(1,9),ones(1,1),zeros(1,9)];
+figure
+dfs(pulse_wave,0,1,0);
 
 X = fft(pulse_wave);
 X_1 = fftshift(X);
@@ -58,33 +98,10 @@ X_1 = fftshift(X);
 
 fs = 1;
 time_domain = -fs/2:1/length(X):fs/2-1/length(X);
+figure
+plot(time_domain,abs(X_1),'-');
 
-plot(time_domain,abs(X_1),'-o');
-
-% Part B
-
-pulse_wave_2 = [zeros(1,10),ones(1,8),zeros(1,10)];
-X = fft(pulse_wave_2);
-X_2 = fftshift(X);
-
-fs = 2;
-time_domain = -fs/2:1/length(pulse_wave):fs/2-1/length(pulse_wave);
-
-plot(time_domain, abs(X_2),'-o');
-
-% [a0,a,b] = dfs(pulse_wave_2,0,1,0);
-
-%Part C
-
-pulse_wave = [zeros(1,5),ones(1,4),zeros(1,5)];
-X = fft(pulse_wave, 256);
-X_2 = fftshift(X);
-fs = 1;
-time_domain = -fs/2:1/length(X_2):fs/2-1/length(X_2);
-
-plot(time_domain,abs(X_2),'-o');
 return
-
 disp('');
 disp('=========== Problem 4 ==========');
 
