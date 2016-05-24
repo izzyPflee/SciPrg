d25de163cefeb3cbbfa1d49e4de9be7486ed7bd6
Isaac <ipfleegor@eagles.ewu.edu>
2016-05-24 11:52:16 -0700

    add babylonian sqrt finding method

---
+++ Hw8/IhpBabylonian.m
@@ -0,0 +1,22 @@
+function root = IhpBabylonian(n)
+%return the sqrt root the parameter x, using the Babylonian method
+
+%tolerance specified by HW8
+tol = 0.0001;
+
+%set initial guess
+x_prev = 0;
+x_next = 2;
+while(abs(x_next - x_prev) > tol)
+   
+    x_prev = x_next;
+    x_next = 0.5*(x_prev + n/x_prev)
+end
+
+% for i = 1:10
+%     x_prev = x_next;
+%     x_next = 0.5*(x_prev + n/x_prev);
+% end
+
+root = x_next;
+end
