887510d61aeed39c8e4629f14f63ecec92d6a20a
Isaac <ipfleegor@eagles.ewu.edu>
2016-06-01 14:04:53 -0700

    add projection using SVD

---
+++ FinalProject/finalProjectWorking.m
@@ -0,0 +1,31 @@
+
+
+%init workspace
+clc, clear, close all;
+
+%load variables from file
+load mix2016.mat
+
+%pre-process data with PCA using SVD
+
+%principal components are in U
+[U,S,V] = svd(x,0);
+%project data
+Z = U'*x;
+
+
+%generate a m-dimensional random unmixing vector(w)
+%with dimensions between 0 and 2pi
+
+%Generate values from the uniform distribution on the
+% interval [a, b] : r = a + (b-a).*rand(100,1);
+
+%generate 4 random values in range 0 to 2pi
+r = 0 + (2*pi - 0).*rand(4,1); 
+n = norm(r);
+%set w to unit-norm vector
+w = r / n;
+
+%project the data onto the unmixing vector
+y = w'*x;
+
