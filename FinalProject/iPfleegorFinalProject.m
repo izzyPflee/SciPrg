9ec2734625b1c2f2775a147a96c34d03c39d50be
Isaac Pfleegor <ipfleegor@eagles.ewu.edu>
2016-06-06 20:36:17 -0700

    add metadata to final project

--- FinalProject/iPfleegorFinalProject.m
+++ FinalProject/iPfleegorFinalProject.m
@@ -1,19 +1,26 @@
 
+% Isaac Pfleegor
+% CSCD 409, Final Project
+% <06/05/2016>
+%
+
 %init workspace
 clc, clear, close all;
+
+disp('Isaac Pfleegor - CSCD 409 Final Project');
+disp('This assignment was tested in Matlab');
 
 %temp before making a function
 %load variables from file
 load mix2016.mat
 
 %step size for kurtosis
-%values has been taken from online example
 h = 1e-5;
 %step size for gradient ascent
 eta = 2e-1;
 %tolerance limit before stopping
 tol = 1e-5;
-mxi = 400;
+mxi = 200;
 %call ppursuit function
 
-[y,K] = ppursuit2(h,eta,tol,mxi,x);
+[y,K] = ppursuit(h,eta,tol,mxi,x);
